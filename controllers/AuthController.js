a0a6e358f0970654d7f6b071978e30e699e0b447
vtrolinux <vtrolinux@gmail.com>
2021-12-14 15:40:38 -0300

    registerUser flash session

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 const Tought = require('../models/Tought')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 module.exports = class AuthController {
     static async login(req,res){
@@ -8,7 +9,53 @@ module.exports = class AuthController {
     static async loginPost(req,res){
         res.render('auth/login')
     }
-    static async Register(req,res){
+    static async register(req,res){
         res.render('auth/register')
+    }
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body
+    
+        // passwords match validation
+        if (password != confirmpassword) {
+          req.flash('message', 'As senhas não conferem, tente novamente!')
+          res.render('auth/register')
+    
+          return
+        }
+    
+        // email validation
+        const checkIfUserExists = await User.findOne({ where: { email: email } })
+    
+        if (checkIfUserExists) {
+            req.flash('message', 'O e-mail já está em uso!')
+            res.render('auth/register')
+    
+            return
+        }
+    
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+    
+        const user = {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    
+        try {
+
+            const createdUser = await User.create(user)
+            // inicializa sessao
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso!')
+     
+            req.session.save(() => {
+                res.redirect('/')// garante que a sessao seja salva antes de redirecionar para a /
+            })
+           
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
