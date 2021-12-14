83245a576ce8dbf64f424859489d3a0d45d5be9f
vtrolinux <vtrolinux@gmail.com>
2021-12-14 16:04:24 -0300

    login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,13 +3,41 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 
 module.exports = class AuthController {
-    static async login(req,res){
+    static login(req,res){
         res.render('auth/login')
     }
     static async loginPost(req,res){
-        res.render('auth/login')
+
+        const {email, password} = req.body
+
+        // find user
+        const user = await User.findOne({ where: { email: email } })
+
+        if(!user) {
+            res.render('auth/login', {
+            message: 'Usuário não encontrado!',
+            })
+            return
+        }
+        // compare password
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if (!passwordMatch) {
+            res.render('auth/login', {
+            message: 'Senha inválida!',
+            })
+            return
+        }
+        // auth user
+        req.session.userid = user.id
+
+        req.flash('message', 'Login realizado com sucesso!')
+
+        req.session.save(() => {
+        res.redirect('/')
+        })
     }
-    static async register(req,res){
+    static register(req,res){
         res.render('auth/register')
     }
     static async registerPost(req, res) {
