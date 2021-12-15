04337e4f60501150be45631552c9e0f4de6757d5
vtrolinux <vtrolinux@gmail.com>
2021-12-14 23:34:27 -0300

    create Tought

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -3,28 +3,7 @@ const User = require('../models/User')
 
 module.exports = class ToughtController {
     static async dashboard(req, res) {
-        const userId = req.session.userid
-    /*
-        const user = await User.findOne({
-            where: {
-            id: userId,
-        },
-            include: Tought,
-            plain: true,
-        })
-    
-        const toughts = user.Toughts.map((result) => result.dataValues)
-    
-        let emptyToughts = true
-    
-        if (toughts.length > 0) {
-          emptyToughts = false
-        }
-    
-        console.log(toughts)
-        console.log(emptyToughts)
-        //res.render('toughts/dashboard', { toughts, emptyToughts })
-    */
+
         res.render('toughts/dashboard')
     }
     static showToughts(req,res){
@@ -34,6 +13,18 @@ module.exports = class ToughtController {
         res.render('toughts/create')
     }
     static async createToughtSave(req, res){
-        
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid,
+        }
+        try {
+            await Tought.create(tought)
+            req.flash('message', 'Pensamento criado com sucesso!')
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
